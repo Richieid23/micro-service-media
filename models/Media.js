5622e47481fd3aa5030736072327575a7277b5a8
Richieid23 <fitrarizki23@gmail.com>
2022-09-23 20:21:27 +0700

    add media migration and model

---
+++ models/Media.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Media = sequelize.define("Media", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allownull: false,
+    },
+    createdAt: {
+      field: "created_at",
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      field: "updated_at",
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'media'
+  });
+
+  return Media;
+}
